05d713550b5f849e3c2251bfc4db3c1210d97e97
Ignacio Chavez <nashio@gmail.com>
2016-04-03 03:40:10 -0700

    Update jquery.star-rating-svg.js

--- src/jquery.star-rating-svg.js
+++ src/jquery.star-rating-svg.js
@@ -13,13 +13,13 @@
     var pluginName = 'starRating';
     var defaults = {
         totalStars: 5,
-				useFullStars: false,
+	useFullStars: false,
         emptyColor: 'lightgray',
         hoverColor: 'orange',
         activeColor: 'gold',
         useGradient: true,
         readonly: false,
-				disableAfterRate: true,
+	disableAfterRate: true,
         starGradient: {
             start: '#FEF7CD',
             end: '#FF9511'
@@ -86,9 +86,9 @@
             this.executeCallback( rating, this.$el );
             this._state.rating = rating;
 
-						if(this.settings.disableAfterRate){
-							this.$stars.off();
-						}
+	    if(this.settings.disableAfterRate){
+	        this.$stars.off();
+	    }
         },
 
         restoreState: function(){
