59be9ab4bf34cf2d9f295aba46afe845c62ea545
Ignacio Chavez <nashio@gmail.com>
2016-06-19 22:20:27 -0700

    fixes #11, setting rating to zero

--- src/jquery.star-rating-svg.js
+++ src/jquery.star-rating-svg.js
@@ -123,7 +123,7 @@
       var index = $target.index() - ((side === 'left') ? 0.5 : 0);
 
       // pointer is way to the left, rating should be none
-      index = ( index < 0 && (e.offsetX < width / 5) ) ? -1 : index;
+      index = ( index < 0.5 && (e.offsetX < width / 4) ) ? -1 : index;
       return index;
     },
 
